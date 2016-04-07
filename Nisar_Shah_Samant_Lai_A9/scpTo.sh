79a7ef9c93082944c2241925c9bc9b5499e4db64
k0vit <kovit@ccs.neu.edu>
2016-04-07 04:44:47 -0400

    working scp and ssh

--- Nisar_Shah_Samant_Lai_A9/scpTo.sh
+++ Nisar_Shah_Samant_Lai_A9/scpTo.sh
@@ -11,8 +11,8 @@ while IFS="," read II PRIP PUIP NT
 do
 	if [ "$NT" == "C" ]
 	then
-		scp -i ec2key.pem -o StrictHostKeyChecking=no client-0.0.1-SNAPSHOT-jar-with-dependencies.jar ec2-user@$PUIP:~/
+		scp -i ec2key.pem -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" client-0.0.1-SNAPSHOT-jar-with-dependencies.jar ec2-user@$PUIP:~/
 	else 
-		scp -i ec2key.pem -o StrictHostKeyChecking=no SortNode-0.0.1-SNAPSHOT-jar-with-dependencies.jar ec2-user@$PUIP:~/
+		scp -i ec2key.pem -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" SortNode-0.0.1-SNAPSHOT-jar-with-dependencies.jar ec2-user@$PUIP:~/
 	fi
 done < InstanceDetails.csv

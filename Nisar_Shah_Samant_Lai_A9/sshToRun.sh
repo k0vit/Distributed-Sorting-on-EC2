57c26cc05051fe845b1d4b047c6fabb15005e933
Kovit Nisar <kovit@ccs.neu.edu>
2016-04-07 04:51:01 -0400

    working ssh

--- Nisar_Shah_Samant_Lai_A9/sshToRun.sh
+++ Nisar_Shah_Samant_Lai_A9/sshToRun.sh
@@ -32,9 +32,9 @@ do
         if [ "$NT" == "C" ]
         then
                 clientip=$PUIP
-        else 
-                ssh -i ec2key.pem -o StrictHostKeyChecking=no ec2-user@$PUIP "java -jar ~/SortNode-0.0.1-SNAPSHOT-jar-with-dependencies.jar $1 $2 $clusterdetails $accesskey $secretkey 2>&1" > sortnode-$PUIP-output.txt &
+        else
+                ssh -i ec2key.pem -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" ec2-user@$PUIP "java -jar ~/SortNode-0.0.1-SNAPSHOT-jar-with-dependencies.jar $1 $2 $clusterdetails $accesskey $secretkey 2>&1" > sortnode-$PUIP-output.txt &
         fi
 done < InstanceDetails.csv
 
-ssh -i ec2key.pem -o StrictHostKeyChecking=no ec2-user@$clientip "java -jar ~/client-0.0.1-SNAPSHOT-jar-with-dependencies.jar $1 $2 $clusterdetails $accesskey $secretkey test.Main 2>&1" > client-output.txt
+ssh -i ec2key.pem -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" ec2-user@$clientip "java -jar ~/client-0.0.1-SNAPSHOT-jar-with-dependencies.jar $1 $2 $clusterdetails $accesskey $secretkey 2>&1" > client-output.txt

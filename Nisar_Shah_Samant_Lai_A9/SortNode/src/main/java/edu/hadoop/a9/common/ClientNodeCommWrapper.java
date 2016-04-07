1976811799d0bf1dd9546e8ac1fd1c26d68ed1af
naineelshah <naineelshah@gmail.com>
2016-04-07 03:54:14 -0400

    SortNode program integrated, removed unneccessary methods, everything complete but untested and logging not proper

--- Nisar_Shah_Samant_Lai_A9/SortNode/src/main/java/edu/hadoop/a9/common/ClientNodeCommWrapper.java
+++ Nisar_Shah_Samant_Lai_A9/SortNode/src/main/java/edu/hadoop/a9/common/ClientNodeCommWrapper.java
@@ -7,9 +7,9 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class ClientNodeCommWrapper {
 	
-	public static void SendData(String clientIp, String port, String data) throws UnirestException {
+	public static void SendData(String clientIp, String port, String requestUrl, String data) throws UnirestException {
 		log.info(String.format("Sending data: %s", data));
-		Unirest.post("http://" + clientIp + ":" + port).body(data).asString();
+		Unirest.post("http://" + clientIp + ":" + port + "/" + requestUrl).body(data).asString();
 	}
 	
 	private static final Logger log = Logger.getLogger(ClientNodeCommWrapper.class.getName());
--- Nisar_Shah_Samant_Lai_A9/SortNode/src/main/java/edu/hadoop/a9/slave/SortNode.java
+++ Nisar_Shah_Samant_Lai_A9/SortNode/src/main/java/edu/hadoop/a9/slave/SortNode.java
@@ -2,22 +2,58 @@ package edu.hadoop.a9.slave;
 
 import static spark.Spark.post;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
+import edu.hadoop.a9.common.ClientNodeCommWrapper;
 import edu.hadoop.a9.common.S3Wrapper;
 
 public class SortNode {
 	static String accessKey;
 	static String secretKey;
 	static String clientIp;
+	public static final int DRY_BULB_COL = 8;
+	static int TOTAL_NO_OF_SORT_NODES;
+	static Map<String, Long> ipToMaxMap = new HashMap<String, Long>();
+	static Map<String, Long> ipToMinMap = new HashMap<String, Long>();
+	static long MINIMUM_PARTITION;
+	static long MAXIMUM_PARTITION;
+	static String INSTANCE_IP;
+	static long INSTANCE_ID;
+	static ArrayList<String[]> unsortedData = new ArrayList<String[]>();
+	public static final String PORT_FOR_SORT_NODE_COMM = "1234";
+	public static final int NUMBER_OF_REQUESTS_STORED = 20000;
+	public static final String PARTITION_URL = "partitions";
+	public static final String END_URL = "end";
+	static int NO_OF_SORT_NODES_WHERE_DATA_IS_RECEIVED = 0;
 	
 	public static void main(String[] args) {
 		if (args.length != 5) {
@@ -28,22 +64,197 @@ public class SortNode {
 			System.err.println(args.length);
 			System.exit(-1);
 		}
+		
+		String outputS3Path = args[1];
+		String configFilePath = args[2];
 		accessKey = args[3];
 		secretKey = args[4];
+		
 		log.info("Application Initialized");
-		sendSampleDistribution();
 		
-		
-		log.info("Application Finished");
-	}
-	
-	public static void sendSampleDistribution() {
 		try {
+			INSTANCE_IP = InetAddress.getLocalHost().getHostName();
 			BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
 			AmazonS3Client s3client = new AmazonS3Client(awsCredentials);
 			S3Wrapper wrapper = new S3Wrapper(s3client);
-			//TODO Read data from each file and store in memory in local directory.
-			//TODO Client will send the file names that need to be downloaded from S3 and you need to download those files only.
+			
+			//This downloads the config file on the local directory.
+			String configFileName = wrapper.readOutputFromS3(configFilePath, awsCredentials);
+			readFileAndSetProps(configFileName);
+			
+			//This is the first thing node will do as soon as it is up.
+			sendSampleDistribution(wrapper, awsCredentials);
+			
+			readPartitionsFromClient();
+			
+			receiveDataFromOtherSortNodes();
+			
+			checkIfAllDataReceived(outputS3Path, wrapper);
+			
+		} catch (IOException | InterruptedException e) {
+			log.severe(e.getMessage());
+		}
+		
+	}
+	
+	private static void receiveDataFromOtherSortNodes() {
+
+		post("/records", (request, response) -> {
+			String recordList = request.body();
+			String[] records = recordList.split(":");
+			for (String record : records) {
+				unsortedData.add(record.split(","));
+			}
+			response.status(200);
+			response.body("Awesome");
+			return response.body().toString();
+		});		
+		
+	}
+
+	/**
+	 * If All data is received then start sorting the data you have and write it to S3.
+	 */
+	private static void checkIfAllDataReceived(String outputS3Path, S3Wrapper wrapper) {
+		post("/end", (request, response) -> {
+			NO_OF_SORT_NODES_WHERE_DATA_IS_RECEIVED++;
+			if (NO_OF_SORT_NODES_WHERE_DATA_IS_RECEIVED == TOTAL_NO_OF_SORT_NODES) {
+				sortYourOwnData();
+				uploadDataToS3(outputS3Path, wrapper);
+			}
+			return response.body().toString();
+		});
+		
+	}
+
+	private static void uploadDataToS3(String outputS3Path, S3Wrapper wrapper) {
+		ArrayList<String[]> nowSortedData = unsortedData;
+		String fileName = "part-r-" + INSTANCE_ID + ".csv";
+		try {
+			CSVWriter writer = new CSVWriter(new FileWriter(fileName));
+			writer.writeAll(nowSortedData);
+			writer.close();
+			wrapper.uploadFile(fileName, outputS3Path);
+		} catch (IOException e) {
+			log.severe(e.getMessage());
+		}
+	}
+
+	public static void readPartitionsFromClient() {
+		JSONParser parser = new JSONParser();
+		Map<String, Integer> ipToCountOfRequests = new HashMap<String, Integer>();
+		Map<String, StringBuilder> ipToActualRequestString = new HashMap<String, StringBuilder>();
+		
+		post("/partitions", (request, response) -> {
+			// String partitions;
+			response.status(200);
+			response.body("SUCCESS");
+			JSONObject entireJSON = (JSONObject) parser.parse(request.body().toString());
+			JSONArray array = (JSONArray) entireJSON.get("partitions");
+			for (int i = 0; i < array.size(); i++) {
+				JSONObject jsonObject = (JSONObject) array.get(i);
+				Long minimumPartition = (Long) jsonObject.get("min");
+				Long maximumPartition = (Long) jsonObject.get("max");
+				String nodeIp = (String) jsonObject.get("nodeIp");
+				Long instanceId = (Long) jsonObject.get("instanceId");
+				if (nodeIp == INSTANCE_IP) {
+					MAXIMUM_PARTITION = maximumPartition;
+					MINIMUM_PARTITION = minimumPartition;
+					INSTANCE_ID = instanceId;
+				}
+				ipToMaxMap.put(nodeIp, maximumPartition);
+				ipToMinMap.put(nodeIp, minimumPartition);
+			}
+			
+			// Read local data line by line
+			File[] dataFolder = listDirectory(System.getProperty("user.dir"));
+			for (File file : dataFolder) {
+				if (!checkFileExtensionsIsGz(file.getName())) continue;
+				FileInputStream fis = new FileInputStream(file);
+				InputStream gzipStream = new GZIPInputStream(fis);
+				BufferedReader br = new BufferedReader(new InputStreamReader(gzipStream));
+				CSVReader reader = new CSVReader(br);
+				String[] line = null;
+				reader.readNext();
+				while ((line = reader.readNext()) != null) {
+					if (!(line.length < 9) && !line[DRY_BULB_COL].equals("-")) {
+						try {
+							double dryBulbTemp = Double.parseDouble(line[DRY_BULB_COL]);
+							// Check which partition it lies within and send to the sortNode required
+							for (String instanceIp : ipToMaxMap.keySet()) {
+								if (dryBulbTemp >= ipToMinMap.get(instanceIp) && dryBulbTemp <= ipToMaxMap.get(instanceIp)) {
+									if (instanceIp == INSTANCE_IP) {
+										unsortedData.add(line);
+									} else {
+										if (ipToCountOfRequests.get(instanceIp) < NUMBER_OF_REQUESTS_STORED) {
+											ipToCountOfRequests.put(instanceIp, ipToCountOfRequests.get(instanceIp) + 1);
+											ipToActualRequestString.put(instanceIp, 
+													ipToActualRequestString.get(instanceIp).append(":" + line));
+										} else {
+											sendRequestToSortNode(instanceIp, ipToCountOfRequests, ipToActualRequestString);
+										}
+									}
+									break;
+								}
+							}
+						} catch (Exception e) {
+							log.severe("Error: " + e.getMessage());
+						}
+					} 
+				}
+				reader.close();
+				br.close();
+				
+				for(String ipAddress : ipToCountOfRequests.keySet()) {
+					sendRequestToSortNode(ipAddress, ipToCountOfRequests, ipToActualRequestString);
+					// SEND EOF to signal end of file
+					ClientNodeCommWrapper.SendData(ipAddress, PORT_FOR_SORT_NODE_COMM, END_URL, "EOF");
+				}
+				
+			}
+			
+			
+			return response.body().toString();
+		});
+		
+	}
+
+	private static void sendRequestToSortNode(String instanceIp, Map<String, Integer> ipToCountOfRequests, Map<String, StringBuilder> ipToActualRequestString) {
+		StringBuilder sb = ipToActualRequestString.get(instanceIp);
+		ipToActualRequestString.put(instanceIp, new StringBuilder());
+		ipToCountOfRequests.put(instanceIp, 0);
+		String recordList = sb.toString();
+		try {
+			ClientNodeCommWrapper.SendData(instanceIp, PORT_FOR_SORT_NODE_COMM, PARTITION_URL ,recordList);
+		} catch (UnirestException e) {
+			log.severe(e.getMessage());
+		}
+	}
+
+	private static boolean checkFileExtensionsIsGz(String fileName) {
+		String format = fileName.substring(fileName.lastIndexOf(".") + 1);
+		if (format.equals("gz")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private static void readFileAndSetProps(String configFileName) throws NumberFormatException, IOException {
+		FileReader fr = new FileReader(configFileName);
+		BufferedReader br = new BufferedReader(fr);
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			String[] column = line.split(",");
+			if (column[3].equals("S")) {
+				TOTAL_NO_OF_SORT_NODES++;
+			}
+		}
+		br.close();
+	}
+	
+	public static void sendSampleDistribution(S3Wrapper wrapper, BasicAWSCredentials awsCredentials) {
+		try {
 			post("/files", (request, response) -> {
 				//Receive request from client for the files which need to be taken care of
 				clientIp = request.ip();
@@ -85,6 +296,21 @@ public class SortNode {
 				log.severe(e.getMessage());
 			}
 		}
+		return files;
+	}
+	
+	private static void sortYourOwnData() {
+		Collections.sort(unsortedData, new Comparator<String[]>() {
+			@Override
+			public int compare(String[] o1, String[] o2) {
+				return (o1[8].compareTo(o2[8]));
+			}
+		});
+	}
+
+	public static File[] listDirectory(String directoryPath) {
+		File directory = new File(directoryPath);
+		File[] files = directory.listFiles();
 		return files;
 	}
 

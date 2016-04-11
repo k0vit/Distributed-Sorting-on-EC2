72bf7be36cba9761cfa9eae6aa83a736dfe4a908
naineelshah <naineelshah@gmail.com>
2016-04-11 01:44:35 -0400

    Added externalSorter class

---
+++ Nisar_Shah_Samant_Lai_A9/SortNode/src/main/java/edu/hadoop/a9/slave/ExternalSorter.java
@@ -0,0 +1,34 @@
+package edu.hadoop.a9.slave;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.google.code.externalsorting.ExternalSort;
+
+public class ExternalSorter {
+	
+	private final static Logger LOGGER = Logger.getLogger("External Sorter");
+//	public static void main(String[] args) throws IOException {
+//		externalSort("./testdata", "result");
+//	}
+
+	public static void externalSort(String file,String output) throws IOException {
+		List<File> l = ExternalSort.sortInBatch(new File(file), new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				try {
+					return o1.split(",")[8].compareTo(o2.split(",")[8]);
+				} catch (Exception e) {
+					LOGGER.log(Level.WARNING, "corrupted line found: str1:"+o1+"\n str2:"+o2);
+					return 1;
+				}
+			}
+		});
+		ExternalSort.mergeSortedFiles(l, new File(output));
+	}
+}
